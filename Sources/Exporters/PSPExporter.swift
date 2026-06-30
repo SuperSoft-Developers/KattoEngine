a5c883440e4c3837ac7ea9328cdf4f6771e09250
SuperSoft-Developers <begameguidi@gmail.com>
2026-06-29 21:10:25 -0300

    Create PSPExporter.swift

---
+++ Sources/Exporters/PSPExporter.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+final class PSPExporter: Exporter {
+
+    func export(project: String, output: String) {
+
+        print("Exportando \(project) para .pbp")
+
+        // TODO:
+        // Executar PSP SDK
+        // Criar EBOOT.PBP
+    }
+}
